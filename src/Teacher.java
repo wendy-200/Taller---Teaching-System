0a166d4732fd7e66475f9436908373eed8cbb86c
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 18:49:13 -0500

    Update Teacher.java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Teacher {
+public class Teacher extends Person{
+    private String teacherCode;
+    private int yearsOfExperience;
+
+    public Teacher() {}
+
+    public Teacher(String name, String lastName, int age, String teacherCode, int yearsOfExperience) {
+        super(name, lastName, age);
+        this.teacherCode = teacherCode;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public String getTeacherCode() {
+        return teacherCode;
+    }
+
+    public void setTeacherCode(String teacherCode) {
+        this.teacherCode = teacherCode;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
 }
