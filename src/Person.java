2dc4c4b76a6858dfee4d97ab0fa7f7a8871707a2
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 18:46:49 -0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    protected String name;
+    protected String lastName;
+    protected int age;
+
+    public Person() {}
+
+    public Person(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
