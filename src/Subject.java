14d928180e672b289117378009fc4a76bd3e4567
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 18:48:00 -0500

    Update Subject.java

--- src/Subject.java
+++ src/Subject.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class subject {
+import java.util.List;
+
+public class Subject {
+    private String name;
+    private Teacher teacher;
+    private List<Student> studentList;
+
+    public Subject() {
+    }
+
+    public Subject(String name, Teacher teacher, List<Student> studentList) {
+        this.name = name;
+        this.teacher = teacher;
+        this.studentList = studentList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
 }
