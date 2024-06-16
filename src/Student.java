f812cc93479008af069201a0c1d6f73dceff5a5a
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 18:47:18 -0500

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Student {
+public class Student  extends Person {
+    private String studentCode;
+    private int noOfSemester;
+
+    public Student() {}
+
+    public Student(String name, String lastName, int age, String studentCode, int noOfSemester) {
+        super(name, lastName, age);
+        this.studentCode = studentCode;
+        this.noOfSemester = noOfSemester;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public int getNoOfSemester() {
+        return noOfSemester;
+    }
+
+    public void setNoOfSemester(int noOfSemester) {
+        this.noOfSemester = noOfSemester;
+    }
 }
